66a50994a978a367cd173db5e6305e5e9a7f39f3
van.td <trandinhvan2001180274@gmail.com>
2025-10-31 23:55:02 +0700

    test

---
+++ TestProject/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]   
+        public IActionResult GetAll()
+        {
+            string[] listName = ["Cong ty A", "Cong Ty B"];
+            return Ok(listName);
+        }
+    }
+}
